73655bae7ed8930561459582840a6a159a79bc69
Skyfacture <nick@skyfacture.com>
2024-11-01 16:37:10 +1100

    Tried new models format stackbit.config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -7,15 +7,15 @@ export default defineStackbitConfig({
     new GitContentSource({
       rootPath: __dirname,
       contentDirs: [".next/server/pages"], // Adjusted to point to the correct directory
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: ".next/server/pages/{slug}.tsx", // Adjusted to match your file structure
-          fields: [{ name: "title", type: "string", required: true }]
-        }
-      ],
+      models: {
+		Page: {
+			type: 'page',
+			urlPath: '/{slug}',
+			filePath: '.next/server/pages/{slug}.tsx', // Adjusted to match your file structure
+			fields: [{ name: 'title', type: 'string', required: true }],
+    },
+  },
+};
       assetsConfig: {
         referenceType: "static",
         staticDir: "public", // This should match the directory where your images are stored
